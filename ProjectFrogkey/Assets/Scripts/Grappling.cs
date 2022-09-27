749f9544221b779747925b2194dfc83acb89cd42
Jose Miguel Villafranca <gearshead145778@yahoo.com>
2022-09-26 23:17:25 -0500

    Added Sprinting and Slope resistances when climbing up Slopes.
    - Sprinting added to "Left Shirt".
    - Slopes will now give Player resistance when trying to climb them.
    - Grapple mechanic is being worked on.

---
+++ ProjectFrogkey/Assets/Scripts/Grappling.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grappling : MonoBehaviour
+{
+    [Header("References")]
+    private PlayerMovement pm;
+    public Transform cam;
+    public Transform mouthTip;
+    public LayerMask whatIsGrappleable;
+    public LineRenderer lr;
+
+    [Header("Grappling")]
+    public float maxGrappleDistance;
+    public float grappleDelayTime;
+
+    private Vector3 grapplePoint;
+
+    [Header("Cooldown")]
+    public float grapplingCd;
+    private float grapplingCdTimer;
+
+    [Header("Input")]
+    public KeyCode grappleKey = KeyCode.Mouse1;
+
+    private bool grappling;
+
+    private void Start()
+    {
+        pm = GetComponent<PlayerMovement>();
+    }
+
+    void Uodate()
+    {
+        if (Input.GetKeyDown(grappleKey)) StartGrapple();
+        
+        if (grapplingCdTimer > 0)
+        {
+            grapplingCdTimer -= Time.deltaTime;
+        }
+    }
+    private void LateUpdate()
+    {
+        if(grappling)
+        {
+            lr.SetPosition(0, mouthTip.position);
+        }
+    }
+
+
+
+    private void StartGrapple()
+    {
+        if (grapplingCdTimer > 0) return;
+
+        grappling = true;
+
+        RaycastHit hit;
+        if(Physics.Raycast(cam.position, cam.forward, out hit, maxGrappleDistance, whatIsGrappleable))
+        {
+            grapplePoint = hit.point;
+            Invoke(nameof(ExcuteGrapple), grappleDelayTime);
+        }
+        else
+        {
+            grapplePoint = cam.position + cam.forward * maxGrappleDistance;
+
+            Invoke(nameof(StopGrapple), grappleDelayTime);
+        }
+        lr.enabled = true;
+        lr.SetPosition(1, grapplePoint);
+    }
+
+    private void ExcuteGrapple()
+    {
+
+    }
+
+    private void StopGrapple()
+    {
+        grappling = false;
+
+        grapplingCdTimer = grapplingCd;
+
+        lr.enabled = false;
+    }
+
+
+}
