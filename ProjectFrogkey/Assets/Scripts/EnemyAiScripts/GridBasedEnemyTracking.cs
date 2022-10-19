c04c38a340baab1c4e9e5927d60ccac31e203117
Marksman7 <56178417+Marksman7@users.noreply.github.com>
2022-10-18 21:01:57 -0500

    DetectionDistanceTRansfer

--- ProjectFrogkey/Assets/Scripts/EnemyAiScripts/GridBasedEnemyTracking.cs
+++ ProjectFrogkey/Assets/Scripts/EnemyAiScripts/GridBasedEnemyTracking.cs
@@ -6,6 +6,12 @@ public class GridBasedEnemyTracking : MonoBehaviour
 {
     public GameObject FroggyPlayer;
 
+
+    public Transform PlayerFroggy;
+    public int PlayerWithInDistance = 5;
+    private bool PlaySpotted = false;
+    public GameObject[] Markering;
+
     private float Speed = 2.0f;
     private float Xing;
     private float Zing;
@@ -15,58 +21,139 @@ public class GridBasedEnemyTracking : MonoBehaviour
     void Start()
     {
         FroggyPlayer = GameObject.FindGameObjectWithTag("Player");
+        PlayerFroggy = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
+
+    void Update()
+    {
+        SeekPlyaerDimaondWithThird();
+        //MarkerRadius();
+    }
     //This is a graph system, think of it as the x and y charts in gemometry class
 
     void FixedUpdate()
     {
-        Xing = FroggyPlayer.transform.position.x;
-        Zing = FroggyPlayer.transform.position.z;
-
-        PostiveX = FroggyPlayer.transform.position.x <= 0;
-        PostiveY = FroggyPlayer.transform.position.z <= 0;
-
-        if (Xing > transform.position.x + 1)
+        if (PlaySpotted == true)
         {
-            if (Zing > transform.position.z + 1)
+            Xing = FroggyPlayer.transform.position.x;
+            Zing = FroggyPlayer.transform.position.z;
+
+            PostiveX = FroggyPlayer.transform.position.x <= 0;
+            PostiveY = FroggyPlayer.transform.position.z <= 0;
+
+            if (Xing > transform.position.x + 1)
             {
-                transform.Translate(Speed * Time.deltaTime, 0, Speed * Time.deltaTime);
+                if (Zing > transform.position.z + 1)
+                {
+                    transform.Translate(Speed * Time.deltaTime, 0, Speed * Time.deltaTime);
+                }
+                else if (Zing < transform.position.z - 1)
+                {
+                    transform.Translate(Speed * Time.deltaTime, 0, -Speed * Time.deltaTime);
+                }
+                else
+                {
+                    transform.Translate(Speed * Time.deltaTime, 0, 0);
+                }
+            }
+            else if (Xing < transform.position.x - 1)
+            {
+                if (Zing > transform.position.z + 1)
+                {
+                    transform.Translate(-Speed * Time.deltaTime, 0, Speed * Time.deltaTime);
+                }
+                else if (Zing < transform.position.z - 1)
+                {
+                    transform.Translate(-Speed * Time.deltaTime, 0, -Speed * Time.deltaTime);
+                }
+                else
+                {
+                    transform.Translate(-Speed * Time.deltaTime, 0, 0);
+                }
+
+            }
+            else if (Zing > transform.position.z + 1)
+            {
+                transform.Translate(0, 0, Speed * Time.deltaTime);
             }
             else if (Zing < transform.position.z - 1)
             {
-                transform.Translate(Speed * Time.deltaTime, 0, -Speed * Time.deltaTime);
+                transform.Translate(0, 0, -Speed * Time.deltaTime);
             }
-            else
-            {
-                transform.Translate(Speed * Time.deltaTime, 0, 0);
-            }
-        }
-        else if (Xing < transform.position.x - 1)
-        {
-            if (Zing > transform.position.z + 1)
-            {
-                transform.Translate(-Speed * Time.deltaTime, 0, Speed * Time.deltaTime);
-            }
-            else if (Zing < transform.position.z - 1)
-            {
-                transform.Translate(-Speed * Time.deltaTime, 0, -Speed * Time.deltaTime);
-            }
-            else
-            {
-                transform.Translate(-Speed * Time.deltaTime, 0, 0);
-            }
-
-        }
-        else if (Zing > transform.position.z + 1)
-        {
-            transform.Translate(0, 0, Speed * Time.deltaTime);
-        }
-        else if (Zing < transform.position.z - 1)
-        {
-            transform.Translate(0, 0, -Speed * Time.deltaTime);
         }
 
+    }
+
+    void SeekPlayerBasic() //box radius
+    {
+        if (PlayerFroggy.transform.position.x > this.transform.position.x &&
+            PlayerFroggy.transform.position.x < (this.transform.position.x + (PlayerWithInDistance * 2)) &&
+            PlayerFroggy.transform.position.z > (this.transform.position.z - PlayerWithInDistance) &&
+            PlayerFroggy.transform.position.z < (this.transform.position.z + PlayerWithInDistance))
+        {
+            PlaySpotted = true;
+        }
+    }
+
+    void SeekPlyaerDimaond() // Dimanond Detction Radius
+    {
+        if (PlayerFroggy.transform.position.x > this.transform.position.x &&
+           PlayerFroggy.transform.position.x < (this.transform.position.x + (PlayerWithInDistance * 2)) &&
+           PlayerFroggy.transform.position.z > (this.transform.position.z - PlayerWithInDistance) &&
+           PlayerFroggy.transform.position.z < (this.transform.position.z + PlayerWithInDistance) &&
+           ((PlayerFroggy.transform.position.x > (this.transform.position.x + (PlayerWithInDistance * 1.5)) && PlayerFroggy.transform.position.z > (this.transform.position.z + (PlayerWithInDistance * .5))) != true) &&
+           ((PlayerFroggy.transform.position.x > (this.transform.position.x + (PlayerWithInDistance * 1.5)) && PlayerFroggy.transform.position.z < (this.transform.position.z - (PlayerWithInDistance * .5))) != true) &&
+           ((PlayerFroggy.transform.position.x < (this.transform.position.x + (PlayerWithInDistance * .5)) && PlayerFroggy.transform.position.z < (this.transform.position.z - (PlayerWithInDistance * .5))) != true) &&
+           ((PlayerFroggy.transform.position.x < (this.transform.position.x + (PlayerWithInDistance * .5)) && PlayerFroggy.transform.position.z > (this.transform.position.z + (PlayerWithInDistance * .5))) != true)
+           )
+        {
+            PlaySpotted = true;
+        }
+    }
+
+    void SeekPlyaerDimaondWithHalf() // Dimanond with one cormer a Half of the size Detction Radius
+    {
+        if (PlayerFroggy.transform.position.x > this.transform.position.x &&
+            PlayerFroggy.transform.position.x < (this.transform.position.x + (PlayerWithInDistance * 1.5)) &&
+            PlayerFroggy.transform.position.z > (this.transform.position.z - PlayerWithInDistance) &&
+            PlayerFroggy.transform.position.z < (this.transform.position.z + PlayerWithInDistance) &&
+            ((PlayerFroggy.transform.position.x > (this.transform.position.x + (PlayerWithInDistance * 1.25)) && PlayerFroggy.transform.position.z > (this.transform.position.z + (PlayerWithInDistance * .5))) != true) &&
+            ((PlayerFroggy.transform.position.x > (this.transform.position.x + (PlayerWithInDistance * 1.25)) && PlayerFroggy.transform.position.z < (this.transform.position.z - (PlayerWithInDistance * .5))) != true) &&
+            ((PlayerFroggy.transform.position.x < (this.transform.position.x + (PlayerWithInDistance * .5)) && PlayerFroggy.transform.position.z < (this.transform.position.z - (PlayerWithInDistance * .5))) != true) &&
+            ((PlayerFroggy.transform.position.x < (this.transform.position.x + (PlayerWithInDistance * .5)) && PlayerFroggy.transform.position.z > (this.transform.position.z + (PlayerWithInDistance * .5))) != true)
+            )
+        {
+            PlaySpotted = true;
+        }
+
+
+    }
+
+    void SeekPlyaerDimaondWithThird() // Dimanond with one cormer a third of the size Detction Radius
+    {
+        if (PlayerFroggy.transform.position.x > this.transform.position.x &&
+            PlayerFroggy.transform.position.x < (this.transform.position.x + (PlayerWithInDistance * 1.25)) &&
+            PlayerFroggy.transform.position.z > (this.transform.position.z - PlayerWithInDistance) &&
+            PlayerFroggy.transform.position.z < (this.transform.position.z + PlayerWithInDistance) &&
+            ((PlayerFroggy.transform.position.x > (this.transform.position.x + (PlayerWithInDistance * 1.125)) && PlayerFroggy.transform.position.z > (this.transform.position.z + (PlayerWithInDistance * .5))) != true) &&
+            ((PlayerFroggy.transform.position.x > (this.transform.position.x + (PlayerWithInDistance * 1.125)) && PlayerFroggy.transform.position.z < (this.transform.position.z - (PlayerWithInDistance * .5))) != true) &&
+            ((PlayerFroggy.transform.position.x < (this.transform.position.x + (PlayerWithInDistance * .5)) && PlayerFroggy.transform.position.z < (this.transform.position.z - (PlayerWithInDistance * .5))) != true) &&
+            ((PlayerFroggy.transform.position.x < (this.transform.position.x + (PlayerWithInDistance * .5)) && PlayerFroggy.transform.position.z > (this.transform.position.z + (PlayerWithInDistance * .5))) != true)
+            )
+        {
+            PlaySpotted = true;
+        }
+
+
+    }
+
+    void MarkerRadius()
+    {
+        Markering[0].transform.position = new Vector3(this.transform.position.z + PlayerWithInDistance, this.transform.position.y, this.transform.position.x + PlayerWithInDistance);
+        Markering[1].transform.position = new Vector3(this.transform.position.z - PlayerWithInDistance, this.transform.position.y, this.transform.position.x + PlayerWithInDistance);
+        Markering[2].transform.position = new Vector3(this.transform.position.z, this.transform.position.y, this.transform.position.x + ((PlayerWithInDistance / 4) + PlayerWithInDistance));
+        Markering[3].transform.position = new Vector3(this.transform.position.z, this.transform.position.y, this.transform.position.x);
     }
 
 }
