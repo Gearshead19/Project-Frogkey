24c78100e7a5da4ae7c2035c703b8de8ea3e29c7
Marksman7 <56178417+Marksman7@users.noreply.github.com>
2022-10-21 11:11:40 -0500

    AiPrefabSTuff

--- ProjectFrogkey/Assets/Scripts/EnemyAiScripts/ProjectileBeingShoot.cs
+++ ProjectFrogkey/Assets/Scripts/EnemyAiScripts/ProjectileBeingShoot.cs
@@ -6,7 +6,7 @@ public class ProjectileBeingShoot : MonoBehaviour
 {
     public float SpeedXDirection = 0.0f;
     public float SpeedZdirection = 30.0f;
-    public float DelayOutOfBoundProtical = 100.0f;
+    public float DelayOutOfBoundProtical = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
