4ba65f2bcace88c6b9cefb91ba8b09339fec80d4
Marksman7 <56178417+Marksman7@users.noreply.github.com>
2022-10-21 10:54:13 -0500

    PlayerHealth

---
+++ ProjectFrogkey/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int HealthPoints = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Projectile"))
+        {
+            Debug.Log("I got shot");
+            Destroy(other.gameObject);
+            this.HealthPoints = this.HealthPoints - 1;
+
+        }
+
+        if(other.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("I got harassed");
+            Destroy(other.gameObject);
+            this.HealthPoints = this.HealthPoints - 1;
+        }
+    }
+}
