b73145f7e67e720fce6f0167d1ece4de139bc514
Jose Miguel Villafranca <gearshead145778@yahoo.com>
2022-12-12 00:45:26 -0600

    Added lots of stuff!!! Will discuss in meeting!

---
+++ ProjectFrogkey/Assets/Scripts/Pixel Crushers/Dialogue System/Scripts/Options/Timeline/Playables/PlaySequence/PlaySequenceBehaviour.cs
@@ -0,0 +1,29 @@
+// Recompile at 9/26/2022 1:21:30 PM
+
+
+#if USE_TIMELINE
+#if UNITY_2017_1_OR_NEWER
+// Copyright (c) Pixel Crushers. All rights reserved.
+
+using UnityEngine;
+using UnityEngine.Playables;
+using System;
+
+namespace PixelCrushers.DialogueSystem
+{
+
+    [Serializable]
+    public class PlaySequenceBehaviour : PlayableBehaviour
+    {
+
+        [Tooltip("Play this sequence.")]
+        [TextArea(5, 5)]
+        public string sequence;
+
+        [Tooltip("(Optional) The other subject in the sequence.")]
+        public Transform listener;
+
+    }
+}
+#endif
+#endif
