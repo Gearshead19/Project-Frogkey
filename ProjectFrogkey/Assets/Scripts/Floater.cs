099ca6af84227dbed677026b937ce32edd25f92d
Madman283 <93745722+Madman283@users.noreply.github.com>
2023-03-10 00:38:35 -0600

    UF_updates

--- ProjectFrogkey/Assets/Scripts/Floater.cs
+++ ProjectFrogkey/Assets/Scripts/Floater.cs
@@ -7,10 +7,13 @@ public class Floater : MonoBehaviour
     public Rigidbody rb;
     public float depthBeforeSubmerged = 1f;
     public float displacementAmount = 3f;
+    public float range;
 
     private void FixedUpdate()
     {
-        if (transform.position.y < 0f)
+        //float waveHeight = WaveManager.instance.GetWaveHeight(transform.position.x);
+
+        if (transform.position.y < range)//waveHeight)
         {
             float displacementMultiplyer = Mathf.Clamp01(-transform.position.y / depthBeforeSubmerged) * displacementAmount;
             rb.AddForce(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * displacementMultiplyer, 0f), ForceMode.Acceleration);
--- ProjectFrogkey/Assets/Scripts/WaveManager.cs
+++ ProjectFrogkey/Assets/Scripts/WaveManager.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class WaveManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+  public static WaveManager instance;
+
+    public float amplitude = 1f;
+    public float length = 2f;
+    public float speed = 1f;
+    public float offset = 0f;
+
+    private void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Debug.Log("Instance already exists, destroying object!");
+            Destroy(this);
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-        
+        offset += Time.deltaTime * speed;
     }
+
+    public float GetWaveHeight(float _x)
+    {
+        return amplitude * Mathf.Sin(_x / length + offset);
+    }
+
+
 }
