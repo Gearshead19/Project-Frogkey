1429cda710da090bdb6544bd43dd8e04de7fedae
adrianggc00 <375191@colum.edu>
2023-03-10 11:52:33 -0600

    Adrain Stuff Again

---
+++ ProjectFrogkey/Assets/Adrain/Assets/Scripts/Target.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float health = 50f;
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        if (health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
