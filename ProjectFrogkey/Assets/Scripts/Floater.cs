db71cd7638978fe668c8ab1a8ab79c6d3c3ae3f8
Madman283 <93745722+Madman283@users.noreply.github.com>
2023-03-09 23:15:25 -0600

    UG_updates

--- ProjectFrogkey/Assets/Scripts/Floater.cs
+++ ProjectFrogkey/Assets/Scripts/Floater.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Floater : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Rigidbody rb;
+    public float depthBeforeSubmerged = 1f;
+    public float displacementAmount = 3f;
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (transform.position.y < 0f)
+        {
+            float displacementMultiplyer = Mathf.Clamp01(-transform.position.y / depthBeforeSubmerged) * displacementAmount;
+            rb.AddForce(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * displacementMultiplyer, 0f), ForceMode.Acceleration);
+        }
     }
 }
