db8d0f21a950539af8d07369d17789ad0ae12ca7
Naikym <mepower2001@gmail.com>
2023-03-17 10:34:13 -0500

    Moved Main town to its folder

---
+++ ProjectFrogkey/Assets/Scripts/Serialization_Save_Scripts/SaveData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+[System.Serializable]
+public class SaveData
+{
+    
+    private static SaveData _current;
+    public static SaveData current
+    {
+        get
+        {
+            if (_current == null)
+            {
+                _current = new SaveData();
+            }
+            return _current;
+        }
+    }
+
+    public PlayerProfile profile;
+}
+
