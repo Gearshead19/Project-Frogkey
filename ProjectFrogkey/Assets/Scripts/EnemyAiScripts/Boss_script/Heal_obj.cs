c63f4a34bd604eb60d39df5991165e1e63499add
Marksman7 <56178417+Marksman7@users.noreply.github.com>
2023-05-09 17:58:16 -0500

    UPdate

--- ProjectFrogkey/Assets/Scripts/EnemyAiScripts/Boss_script/Heal_obj.cs
+++ ProjectFrogkey/Assets/Scripts/EnemyAiScripts/Boss_script/Heal_obj.cs
@@ -7,15 +7,29 @@ public class Heal_obj : MonoBehaviour
 
     public GameObject spawn_boss;
 
+    private GameObject froggy_player;
+
+    public float dis_for_actiavtion = 6;
+
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(spawn_boss, this.transform);
+        froggy_player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (spawn_boss != null)
+        {
+
         
+            if (Vector3.Distance(this.transform.position, froggy_player.transform.position) < dis_for_actiavtion)
+            {
+
+                Instantiate(spawn_boss, this.transform);
+            }
+        }
+
     }
 }
